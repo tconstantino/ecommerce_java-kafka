70a0fa8e4bc7e8463fbffd25605e446442bf9c65
thiago constantino <thiagoconstantino@thiagos-MacBook-Pro.local>
2023-03-31 15:09:00 -0300

    Refactoring consumidores FraudDetectorService e EmailService

---
+++ src/main/java/KafkaService.java
@@ -0,0 +1,41 @@
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.UUID;
+
+public class KafkaService {
+    private final KafkaConsumer<String, String> consumer;
+    private final ConsumerFunction parse;
+
+    public KafkaService(String groupId, String topic, ConsumerFunction parse) {
+        this.parse = parse;
+        this.consumer = new KafkaConsumer<>(properties(groupId));
+        consumer.subscribe(Collections.singletonList(topic));
+    }
+
+    public void run() {
+        while (true) {
+            var records = consumer.poll(Duration.ofMillis(100));
+            if (!records.isEmpty()) {
+                System.out.println("Encontrei " + records.count() + " registros");
+                records.forEach((record) -> {
+                    parse.consume(record);
+                });
+            }
+        }
+    }
+
+    private static Properties properties(String groupId) {
+        var properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, "1");
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, UUID.randomUUID().toString());
+        return properties;
+    }
+}
