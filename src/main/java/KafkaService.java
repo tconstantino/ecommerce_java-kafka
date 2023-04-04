3069df5c54cc970a542131cf1af86072c68a7665
thiago constantino <thiagoconstantino@thiagos-MacBook-Pro.local>
2023-04-03 23:17:09 -0300

    Refactoring LogService

--- src/main/java/KafkaService.java
+++ src/main/java/KafkaService.java
@@ -7,18 +7,28 @@ import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
 import java.util.UUID;
+import java.util.regex.Pattern;
 
 public class KafkaService implements Closeable {
     private final KafkaConsumer<String, String> consumer;
     private final ConsumerFunction parse;
 
     public KafkaService(String groupId, String topic, ConsumerFunction parse) {
-        this.parse = parse;
-        this.consumer = new KafkaConsumer<>(properties(groupId));
+        this(groupId, parse);
         consumer.subscribe(Collections.singletonList(topic));
     }
 
-     void run() {
+    public KafkaService(String groupId, Pattern topic, ConsumerFunction parse) {
+        this(groupId, parse);
+        consumer.subscribe(topic);
+    }
+
+    private KafkaService(String groupId, ConsumerFunction parse) {
+        this.parse = parse;
+        this.consumer = new KafkaConsumer<>(properties(groupId));
+    }
+
+    void run() {
         while (true) {
             var records = consumer.poll(Duration.ofMillis(100));
             if (!records.isEmpty()) {
