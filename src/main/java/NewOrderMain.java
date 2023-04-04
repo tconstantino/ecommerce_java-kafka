ce645179e5ecf19a62656472e0e3f658bcde8ee2
thiago constantino <thiagoconstantino@thiagos-MacBook-Pro.local>
2023-04-03 23:04:24 -0300

    Serializando mensagem para objetos genericos

--- src/main/java/NewOrderMain.java
+++ src/main/java/NewOrderMain.java
@@ -1,16 +1,22 @@
+import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        try(var dispatcher = new KafkaDispatcher()) {
+        try(var orderDispatcher = new KafkaDispatcher<Order>();
+            var emailDispatcher = new KafkaDispatcher<String>()) {
             for (int i = 0; i < 10; i++) {
                 var key = UUID.randomUUID().toString();
-                var order = key + ",67523,1234";
-                dispatcher.send("ECOMMERCE_NEW_ORDER", key, order);
+
+                var userId = UUID.randomUUID().toString();
+                var orderId = UUID.randomUUID().toString();
+                var amount = new BigDecimal(Math.random() * 5000 + 1);
+                var order = new Order(userId, orderId, amount);
+                orderDispatcher.send("ECOMMERCE_NEW_ORDER", key, order);
 
                 var email = "Thank you for your order! We are processing your order!";
-                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+                emailDispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
             }
         }
     }
---
+++ src/main/java/Order.java
@@ -0,0 +1,14 @@
+import org.apache.kafka.common.protocol.types.Field;
+
+import java.math.BigDecimal;
+
+public class Order {
+    public Order(String userId, String orderId, BigDecimal amount) {
+        this.userId = userId;
+        this.orderId = orderId;
+        this.amount = amount;
+    }
+    private final String userId;
+    private final String orderId;
+    private final BigDecimal amount;
+}
